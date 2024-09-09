600915821e9ba39a72708f486557c53384533c2d
salif18 <salifmoctarkonate@gmail.com>
2024-09-09 20:11:37 +0000

    j

--- routes/route_products.js
+++ routes/route_products.js
@@ -6,7 +6,7 @@ const middleware = require("../middlewares/AuthMiddleware");
 const uploadFile = require("../middlewares/ImageMiddleware")
 
 Router.post("/",middleware,uploadFile,Product_Controller.create);
-Router.get("/:userId",middleware,Product_Controller.getProduits);
+Router.get("/:userId",Product_Controller.getProduits);
 Router.get("/single/:id",middleware,Product_Controller.getOneProduits);
 Router.put("/single/:id",middleware,uploadFile,Product_Controller.update);
 Router.delete("/single/:id",middleware,Product_Controller.delete);
