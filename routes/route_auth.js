4109d81244c81c30db047176cc58b547b8ac9405
salif18 <salifmoctarkonate@gmail.com>
2024-09-13 10:45:51 +0000

    modification de signup a registre

--- routes/route_auth.js
+++ routes/route_auth.js
@@ -5,7 +5,7 @@ const Auth_Controller = require("../controller/auth_controller");
 const middleware = require("../middlewares/AuthMiddleware");
 
 
-Router.post("/signup",Auth_Controller.registre);
+Router.post("/registre",Auth_Controller.registre);
 Router.post("/login",Auth_Controller.login);
 Router.post("/logout",middleware,Auth_Controller.logout);
 
