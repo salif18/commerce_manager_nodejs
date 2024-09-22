e869db4bf00005355df4ffaf45778342947e33c1
salif18 <salifmoctarkonate@gmail.com>
2024-09-22 22:18:38 +0000

    ajout dautres format de fichier

--- middlewares/multercloudinar.js
+++ middlewares/multercloudinar.js
@@ -5,7 +5,7 @@ module.exports = multer({
   storage:multer.diskStorage({}),
   fileFilter:(req,file,cb)=>{
     let ext = path.extname(file.originalname);
-    if(ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png"){
+    if(ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png" && ext !== ".webp" && ext !== ".svg"){
       cb(new Error("erreur de type de fichier"),false);
       return ;
     }
