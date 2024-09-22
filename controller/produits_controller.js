7e7ac6c1d8fc12851a197c9060b2ec7b70d59acb
salif18 <salifmoctarkonate@gmail.com>
2024-09-22 00:00:42 +0000

    changement de cloudinary avec local

--- controller/produits_controller.js
+++ controller/produits_controller.js
@@ -3,11 +3,12 @@ const fs = require('fs');
 
 exports.create = async (req, res, next) => {
     try {
+      
         // Création d'un nouvel objet produit
         const nouveauProduit = new Produits({
             ...req.body,
-            // image: req.file ? `${req.protocol}://${req.get("host")}/images/${req.file.filename}` : "",
-            image: req.file ? req.file.path : "", // URL Cloudinary renvoyée dans req.file.path
+            image: req.file ? `${req.protocol}://${req.get("host")}/images/${req.file.filename}` : "",
+            // image: req.file ? req.file.path : "", // URL Cloudinary renvoyée dans req.file.path
             userId: req.auth.userId // Associer le produit à l'utilisateur
         });
 
@@ -88,8 +89,8 @@ exports.update = async (req, res) => {
             id,
             {
                 nom: nom ? nom : produit.nom,
-                // image: req.file ? `${req.protocol}://${req.get("host")}/images/${req.file.filename}` : produit.image,
-                image: req.file ? req.file.path : produit.image , // URL Cloudinary renvoyée dans req.file.path
+                image: req.file ? `${req.protocol}://${req.get("host")}/images/${req.file.filename}` : produit.image,
+                // image: req.file ? req.file.path : produit.image , // URL Cloudinary renvoyée dans req.file.path
                 categories: categories.length > 0 ? categories : produit.categories,
                 prix_achat: prix_achat.length > 0 ? prix_achat : produit.prix_achat,
                 prix_vente: prix_vente.length > 0 ? prix_vente : produit.prix_vente,
@@ -125,32 +126,32 @@ exports.delete = async (req, res) => {
             return res.status(401).json({ message: 'Non autorisé' });
         }
 
-        // const filename = produit.image.split('/images/')[1];
+        const filename = produit.image.split('/images/')[1];
 
-        // // Supprimer l'image du serveur
-        // fs.unlink(`public/images/${filename}`, async (err) => {
-        //     if (err) {
-        //         return res.status(500).json({ message: "Erreur lors de la suppression de l'image", error: err });
-        //     }
+        // Supprimer l'image du serveur
+        fs.unlink(`public/images/${filename}`, async (err) => {
+            if (err) {
+                return res.status(500).json({ message: "Erreur lors de la suppression de l'image", error: err });
+            }
 
-        //     // Supprimer le produit après avoir supprimé l'image
-        //     await produit.deleteOne({ _id: id });
-        //     return res.status(200).json({ message: 'Produit supprimé avec succès' });
-        // });
+            // Supprimer le produit après avoir supprimé l'image
+            await produit.deleteOne({ _id: id });
+            return res.status(200).json({ message: 'Produit supprimé avec succès' });
+        });
 
          // Extraire l'identifiant public de l'image sur Cloudinary
-         const publicId = produit.image.split('/').pop().split('.')[0]; // Obtient l'ID public de l'image Cloudinary
+        //  const publicId = produit.image.split('/').pop().split('.')[0]; // Obtient l'ID public de l'image Cloudinary
 
-         // Supprimer l'image de Cloudinary
-         cloudinary.uploader.destroy(publicId, async (err, result) => {
-             if (err) {
-                 return res.status(500).json({ message: "Erreur lors de la suppression de l'image", error: err });
-             }
+        //  // Supprimer l'image de Cloudinary
+        //  cloudinary.uploader.destroy(publicId, async (err, result) => {
+        //      if (err) {
+        //          return res.status(500).json({ message: "Erreur lors de la suppression de l'image", error: err });
+        //      }
  
-             // Si l'image est supprimée avec succès, supprimer le produit
-             await produit.deleteOne({ _id: id });
-             return res.status(200).json({ message: 'Produit et image supprimés avec succès' });
-         });
+        //      // Si l'image est supprimée avec succès, supprimer le produit
+        //      await produit.deleteOne({ _id: id });
+        //      return res.status(200).json({ message: 'Produit et image supprimés avec succès' });
+        //  });
  
 
     } catch (err) {
--- middlewares/ImageMiddleware.js
+++ middlewares/ImageMiddleware.js
@@ -1,46 +1,45 @@
+const multer = require('multer');
+const path = require('path');
+
+const MIME_TYPES = {
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpg',
+  'image/png': 'png', 
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null,path.join(__dirname, '../public/images'));
+  },
+  filename: (req, file, callback) => {
+    const name = file.originalname.split(' ').join('_');
+    const extension = MIME_TYPES[file.mimetype];
+    callback(null, name + Date.now() + '.' + extension);
+  }
+});
+
+module.exports = multer({storage: storage}).single('image');
+// require("dotenv").config()
+// const cloudinary = require('cloudinary').v2;
+// const { CloudinaryStorage } = require('multer-storage-cloudinary');
 // const multer = require('multer');
-// const path = require('path');
 
-// const MIME_TYPES = {
-//   'image/jpg': 'jpg',
-//   'image/jpeg': 'jpg',
-//   'image/png': 'png', 
-// };
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, callback) => {
-//     callback(null,path.join(__dirname, '../public/images'));
-//   },
-//   filename: (req, file, callback) => {
-//     const name = file.originalname.split(' ').join('_');
-//     const extension = MIME_TYPES[file.mimetype];
-//     callback(null, name + Date.now() + '.' + extension);
-//   }
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
 // });
 
-// module.exports = multer({storage: storage}).single('image');
-require("dotenv").config()
-const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const multer = require('multer');
+// const storage = new CloudinaryStorage({
+//   cloudinary: cloudinary,
+//   params: {
+//     folder: 'produits', // Vous pouvez choisir un nom de dossier
+//     format: async (req, file) => {
+//       const ext = file.mimetype.split('/')[1];
+//       return ext === 'jpeg' ? 'jpg' : ext; // convertit les fichiers jpeg en jpg
+//     },
+//     public_id: (req, file) => file.originalname.split(' ').join('_') + Date.now(), // Nom unique basé sur l'original et le timestamp
+//   },
+// });
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
-  params: {
-    folder: 'produits', // Vous pouvez choisir un nom de dossier
-    format: async (req, file) => {
-      const ext = file.mimetype.split('/')[1];
-      return ext === 'jpeg' ? 'jpg' : ext; // convertit les fichiers jpeg en jpg
-    },
-    public_id: (req, file) => file.originalname.split(' ').join('_') + Date.now(), // Nom unique basé sur l'original et le timestamp
-  },
-});
-
-const upload = multer({ storage: storage }).single('image');
-module.exports =upload
+// module.exports = multer({ storage: storage }).single('image');
